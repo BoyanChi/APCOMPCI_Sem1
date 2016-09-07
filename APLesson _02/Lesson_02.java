b80a82d80b2d4604012bcb47eb9132daab6e1031
BoyanChi <ChiB8809@sduhsd.lan>
2016-09-07 14:26:21 -0700

    Finish the Lesson 02 assignment

---
+++ APLesson _02/Lesson_02.java
@@ -0,0 +1,30 @@
+public class Lesson_02
+{
+	public static void main(String[]args)
+	{
+		int a = 7;
+		int b = 9;
+		
+		System.out.println(a*b);
+		
+		
+		String the = "3710";
+		String address = "Del Mar";
+		String of = "Heights Road";
+		String TP = "SanDiego";
+		String HS = "CA";
+		String is ="92130";
+		
+		System.out.println(the + " " + address + " " + of + " " + TP + " " + HS + " " + is);
+		
+		
+		int l = 2;
+		int w = 3;
+		int h = 4;
+		int n = 2;
+		int SArea = n*w*h+n*w*l+n*h*l;
+		
+		System.out.println("The surface area of your rectangle is" + " " +SArea);
+	
+	}
+}
