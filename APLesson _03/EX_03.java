78d25d209da371ddc877a8ade4ad0b91c7545972
BoyanChi <1009365729@qq.com>
2016-09-15 14:10:34 -0700

    Refined EX_03

--- APLesson _03/EX_03.java
+++ APLesson _03/EX_03.java
@@ -5,15 +5,12 @@ public class EX_03
 	public static void main(String [] args)
 	{
 		
-		//instantiates the new Scanner object "keyboard"
 		Scanner keyboard = new Scanner(System.in);
 		
 		
-		//prompts for user input
 		System.out.println("Please tell me your height in meters.");
+		double t = keyboard.nextDouble();
 		
-		//search for the next integer that the user enters
-		int t = keyboard.nextInt();
 	
 		double height = t*3.28;
 		
