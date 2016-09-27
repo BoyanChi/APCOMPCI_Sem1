2781bd958022b90faf569bcc4e1440c3819a0371
BoyanChi <1009365729@qq.com>
2016-09-27 13:43:38 -0700

    Refined lesson 2

--- APLesson _02/Lesson_02.java
+++ APLesson _02/Lesson_02.java
@@ -5,17 +5,16 @@ public class Lesson_02
 		int a = 7;
 		int b = 9;
 		
-		System.out.println("a multiplies by b equal to" + a*b);
+		System.out.println("a multiplies by b equal to " + a*b);
 		
 		
 		String the = "3710";
-		String address = "Del Mar";
-		String of = "Heights Road";
+		String address = "Del Mar Heights Road";
 		String TP = "SanDiego";
 		String HS = "CA";
 		String is ="92130";
 		
-		System.out.println(the + "," + address + " " + of\nTP\nHS + "," + is);
+		System.out.println(the + "," + address  + " \n" + TP + " \n" + HS + "," + is);
 		
 		
 		int l = 2;
