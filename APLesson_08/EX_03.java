13f868053a3622369391ffa8f6cd411434a8b62e
BoyanChi <1009365729@qq.com>
2016-11-10 14:19:10 -0800

    2

--- APLesson_08/EX_03.java
+++ APLesson_08/EX_03.java
@@ -6,3 +6,19 @@ public class EX_03
 		Scanner keyboard = new Scanner(System.in);
 		System.out.println("Please enter one number.");
 		int num = keyboard.nextInt();
+		System.out.println(luck(num));
+	}
+	public static int luck(int num)
+	{
+		if(num >= 0)
+		{
+			if(num % 10 == 7)
+				return 1 + luck(num/10);
+			
+			else
+				return 0 + luck(num/10);
+		}
+		else
+			return 0;
+	}
+}
---
+++ APLesson_08/EX_04.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class EX_04
+{
+	public static void main(String[]args)
+	{
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("Please enter a word.");
+		String word = keyboard.next();
+		int stop = word.length();
+		int start = 0;
+		System.out.println(tree(word,start,stop));
+	}
+	public static String tree(String word, int start, int stop)
+	{
+		if(start <= stop)
+			word.substring(0,start);
+			start ++;
+			return tree(word,start,stop);
+	}
+}
+		
