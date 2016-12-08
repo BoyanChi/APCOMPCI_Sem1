76312aa43e7bdcb7a6ba316792fd6787dbf89748
BoyanChi <1009365729@qq.com>
2016-12-08 13:47:40 -0800

    1

--- APLesson_08/EX_04.java
+++ APLesson_08/EX_04.java
@@ -8,19 +8,19 @@ public class EX_04
 		System.out.println("Please enter a word.");
 		String word = keyboard.next();
 		int stop = word.length();
-		System.out.println("" + tree(word, start, stop));
+		System.out.println(tree(word, start, stop));
 	}
 	public static String tree(String word, int start, int stop)
 	{
 		if(start <= stop)
 		{
-			System.out.println(word.substring(0,start));
+			System.out.printf("%10s\n", word.substring(0,start));
 			start ++;
-			return word;
+			return tree(word, start, stop);
 		}
 		else
 		{
-			return word;
+			return tree(word, start, stop);
 		}
 		
 	}
