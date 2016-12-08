12f3ca5787cdf03783f7f8313aa7a5e207ae617a
BoyanChi <1009365729@qq.com>
2016-12-08 13:39:06 -0800

    2

--- APLesson_10/EX_01.java
+++ APLesson_10/EX_01.java
@@ -4,39 +4,35 @@ import java.util.Arrays;
 public class EX_01
 {
 	static Scanner keyboard = new Scanner(System.in);
+	static int n;
 	public static void main(String[]args)
 	{
 		Integer [] numbers = {2, 6, 8, 9, 10, 12, 13, 15, 17, 24, 55, 66, 78, 77, 79};
 		ArrayList<Integer> nums = new ArrayList<> (Arrays.asList(numbers));
-		gFactor(nums);
 		removePrimes(nums);
+		System.out.print(nums);
 	}
-	public static int gFactor(ArrayList<Integer> nums)
+	public static int gFactor(int n)
 	{
-		System.out.println("Please enter a number as parameter");
-		int n = keyboard.nextInt(); 
-		for (int i = 2; i <= n; i++)
+		for (int i = 2; i < n; i++)
 		{
 			if((n % i) == 0)
 			{
 				return 1;
 			}
-			else
-			{
-				return 0;
-			}
 		}
+		return 0;
+		
 	}
 	public static void removePrimes(ArrayList<Integer> nums)
 	{
-		for (int i = 0; i <= nums.size(); i++)
+		for (int i = 0; i < nums.size(); i++)
 		{
-			if (gFactor.get(i) == 0)
+			if (gFactor(nums.get(i)) == 0)
 			{				
 				nums.remove(i);
 				i -= 1;
-			}
-			System.out.print(" " + i + " ");	
+			}			
 		}
 	}
 }
