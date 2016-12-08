75a44fecf4c7da34113e6eb6845dffb84c466341
BoyanChi <1009365729@qq.com>
2016-12-08 14:29:10 -0800

    1

---
+++ APLesson_11/EX_01.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+public class EX_01
+{
+	static Scanner keyboard = new Scanner(System.in);
+	public static void main(String[]args)
+	{
+		int[][]nums = new int [4][4];
+		for(int i = 0; i < numbers.length; i++)
+		{
+			num[i][i] = (int)(Math.random()*100)+1;
+		}
+	}
+}
