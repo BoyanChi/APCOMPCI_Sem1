032afc9e752d3fe864e60a58c4e688ecdab95dc7
BoyanChi <1009365729@qq.com>
2017-02-07 13:55:04 -0800

    1

--- APLesson_13/ToyStore.java
+++ APLesson_13/ToyStore.java
@@ -4,6 +4,12 @@ public class ToyStore
 	ArrayList<toy> Toy = new ArrayList<toy>();
 	public ToyStore()
 	{
-		
+		String[]toys;
+		for(int i = 0, 0 < i < toys.size)
+		{
+			String name = toys[i];
+			String type = toy[i+1];
+			t
+		}
 	}
 }
