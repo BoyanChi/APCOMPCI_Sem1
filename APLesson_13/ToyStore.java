9e4aea0d4f69d80a7bbdfba1421502df693d27f6
BoyanChi <1009365729@qq.com>
2017-02-09 14:31:14 -0800

    1

--- APLesson_13/ToyStore.java
+++ APLesson_13/ToyStore.java
@@ -2,14 +2,34 @@ import java.util.ArrayList;
 public class ToyStore
 {
 	ArrayList<toy> Toy = new ArrayList<toy>();
-	public ToyStore()
+	public String loadToys()
 	{
 		String[]toys;
 		for(int i = 0, 0 < i < toys.size)
 		{
+			String ts = "";
 			String name = toys[i];
 			String type = toy[i+1];
-			t
+			String Toy = getThatToy(name);
+			{
+				if
+			}
 		}
 	}
+	public String getThatToy(Toy)
+	{
+		 int nm = "";
+		 for(int i = 0, i < Toy.length, i++)
+			if (Toy[i] == 
+				
+	}
+	public String getMostFrequentToy(Toy)
+	{
+		String name = "";
+		int max = 
+		for(int i = 0, i < Toy.length, i++)
+			if(i < count || i = count || name = Toy[i])
+				return name
+	}
+
 }
