fb9b165575638f35e0d053d57bb53cecaa944f4a
BoyanChi <1009365729@qq.com>
2017-03-21 13:48:32 -0700

    1

---
+++ APLesson_14/14.2/GMC.java
@@ -0,0 +1,15 @@
+public class GMC extends Car
+{
+	public GMC()
+	{
+		super();
+	}
+	public GMC(double x, double y)
+	{
+		super(x,y);
+	}
+	public int getID()
+	{
+		return id;
+	}
+}
---
+++ APLesson_14/14.2/Honda.java
@@ -0,0 +1,15 @@
+public class Honda extends Car
+{
+	public Honda()
+	{
+		super();
+	}
+	public Honda(double[] l)
+	{
+		super(l[0], l[1]);
+	}
+	public int getID()
+	{
+		return id;
+	}
+}
